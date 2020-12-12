66fbd3c60ff82754b938fcbc9d243f138ad23a2e
Firwing26 <firewing26_kop@mail.ru>
2020-12-13 01:44:16 +0500

    UPD

--- web/index.php
+++ web/index.php
@@ -42,7 +42,10 @@ $app->post('/', function() use($app) {
                'sekond' => 'да'
              );
 
-             
+             if ($slovo == $problem) {
+                $request_params['message'] = 'dct gkj[j]';
+}
+
              file_get_content('https://api.vk.com/method/messages.send?' . http_build_query($request_params));
 
 
